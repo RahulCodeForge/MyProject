9d5136722b39ba2166452fc8bb0ded1549f988c7
Rahul Kumar <rk9676902@gmail.com>
2026-07-18 01:00:00 +0530

    Data acess through variable in templates

--- myproject5/blog/views.py
+++ myproject5/blog/views.py
@@ -4,5 +4,30 @@ from datetime import datetime
 
 # Create your views here.
 
-def User(request,name,age):
-    pass
+class User:
+
+    def __init__(self,name, age):
+        self.name= name
+        self.age= age
+
+def Home(request):
+
+    context={
+
+        'name':'Rahul kumar',
+        'age':'24',
+        'skill' : ['python', 'sql', 'django'],
+        'user': User('Kumar','30'),
+        'blog':{
+            'title' : 'Django project intro',
+            'content': '<b> This is bold</b>',
+            'created_ at':datetime(2026,7,18,12,3),
+        },
+
+        'empty_value': None,
+    }
+
+    return render(request,'blog/post_data.html',context)
+
+
+
