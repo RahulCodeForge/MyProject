3163b83e161168aca2911ab787255c8c4a97ff33
Rahul Kumar <rk9676902@gmail.com>
2026-07-21 23:51:59 +0530

    template updated

--- myproject6/blog/views.py
+++ myproject6/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 
@@ -7,9 +8,16 @@ def post_details(request):
     post={
         'Name':'Rahul kumar',
         'Age': '24',
+        'title':"My second template post",
+         'descruiption': "Django is high on demand",
+         'author':None,
+         'created_at':datetime(2026,7,21,10,56),
+         'tags':['django','python', 'web development'],
+        
+
 
 
 
     }
 
-    return render(request,'blog/post_details.html',post)
+    return render(request,'blog/post_details.html',{'post':post})
